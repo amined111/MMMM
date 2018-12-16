515515b2d24724968afa169ddd69ecab4eda96ea
amined111 <43893689+amined111@users.noreply.github.com>
2018-12-16 21:18:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,34 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "A";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Aesthetic without any manufacture`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+const prefix = "A";
 
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "426926131808174080") return;
+ 
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/amine12a");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+if (message.content.startsWith(prefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} 
+
+if (message.content.startsWith(prefix + 'setwatch')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} 
+if (message.content.startsWith(prefix + 'setlisteng')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+  
+});
 
 
 
